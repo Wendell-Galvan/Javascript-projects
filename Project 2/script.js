4fd5e2d72dd47d93ceba83f4defc663eaadb998b
Wendell Galvan <you@example.com>
2022-07-09 13:11:52 -0700

    Project 2 submission

---
+++ Project 2/script.js
@@ -0,0 +1,7 @@
+// -Create an HTML select dropdown with at least 5 color options in it
+// use the "onchange" attribute so that every time a user selects a color using the dropdown, 
+// it calls your JavaScript function that changes the background color of the BODY to the color selected in the dropdown
+
+function changeBackgroundColor(col){
+	document.bgColor = col;
+}
