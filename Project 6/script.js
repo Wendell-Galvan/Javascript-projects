92c182d25b9e5574c2fca788e6eb17b939c9c2d7
Wendell Galvan <you@example.com>
2022-08-17 11:32:26 -0700

    Fixed html and JS functions

--- Project 6/script.js
+++ Project 6/script.js
@@ -3,28 +3,31 @@
 // -When the user submits the quiz, add a message underneath each question letting them know if they got it right/wrong 
 // (give them the correct answer).
 
-function answer(){
-	var AT = document.getElementById("alanTuring");
-	var CB = document.getElementById("charlesBabbage");
-	var JH = document.getElementById("johnHerschel");
-	var SJ = document.getElementById("steveJobs");
 
-	if (AT.checked==true){
+function questionOne(){
+	let at = document.getElementById("alanTuring");
+	let cb = document.getElementById("charlesBabbage");
+	let jh = document.getElementById("johnHerschel");
+	let sj = document.getElementById("steveJobs");
+
+	if (at.checked==true){
 		answerOne.textContent = "Incorrect. The answer is Charles Babbage.";
-	} else if (CB.checked==true){
+	} else if (cb.checked==true){
 		answerOne.textContent = "Correct! Charles Babbage invented the first computer although it was never built.";
-	} else if (JH.checked==true){
+	} else if (jh.checked==true){
 		answerOne.textContent = "Incorrect. The answer is Charles Babbage.";
-	} else if (SJ.checked==true){
+	} else if (sj.checked==true){
 		answerOne.textContent = "Incorrect. The answer is Charles Babbage.";
 	} else {
 		answerOne.textContent = "You did not select an answer. Please try again.";
 	}
+}
 
-	var year1 = document.getElementById("1901");
-	var year2 = document.getElementById("1985");
-	var year3 = document.getElementById("1946");
-	var year4 = document.getElementById("1996");
+function questionTwo(){
+	let year1 = document.getElementById("1901");
+	let year2 = document.getElementById("1985");
+	let year3 = document.getElementById("1946");
+	let year4 = document.getElementById("1996");
 
 	if (year1.checked==true){
 		answerTwo.textContent = "Incorrect. The answer is 1946.";
@@ -37,41 +40,48 @@ function answer(){
 	} else {
 		answerTwo.textContent = "You did not select an answer. Please try again.";
 	}
+}
 
-	var AL = document.getElementById("adaLovelace");
-	var SW = document.getElementById("steveWozniak");
-	var GB = document.getElementById("georgeBoole");
-	var GH = document.getElementById("graceHopper");
+function questionThree(){
+	let al = document.getElementById("adaLovelace");
+	let sw = document.getElementById("steveWozniak");
+	let gb = document.getElementById("georgeBoole");
+	let gh = document.getElementById("graceHopper");
 
-	if (AL.checked==true){
+	if (al.checked==true){
 		answerThree.textContent = "Correct!";
-	} else if (SW.checked==true){
+	} else if (sw.checked==true){
 		answerThree.textContent = "Incorrect. Ada Lovelace was the first computer programmer.";
-	} else if (GB.checked==true){
+	} else if (gb.checked==true){
 		answerThree.textContent = "Incorrect. Ada Lovelace was the first computer programmer.";
-	} else if (GH.checked==true){
+	} else if (gh.checked==true){
 		answerThree.textContent = "Incorrect. Ada Lovelace was the first computer programmer.";
 	} else {
 		answerThree.textContent = "You did not select an answer. Please try again.";
 	}
+}
 
+function answer(){
+	questionOne();
+	questionTwo();
+	questionThree();
 }
 
 function resetGame(){
-	var reset1 = document.getElementsByName("ans1");
-	   for(var i=0;i<reset1.length;i++)
+	let reset1 = document.getElementsByName("ans1");
+	   for(let i=0;i<reset1.length;i++)
 	      reset1[i].checked = false;
 	answerOne.textContent ="";
 
 
-	var reset2 = document.getElementsByName("ans2");
-	   for(var i=0;i<reset2.length;i++)
+	let reset2 = document.getElementsByName("ans2");
+	   for(let i=0;i<reset2.length;i++)
 	      reset2[i].checked = false;
 	answerTwo.textContent ="";
 
 
-	var reset3 = document.getElementsByName("ans3");
-	   for(var i=0;i<reset3.length;i++)
+	let reset3 = document.getElementsByName("ans3");
+	   for(let i=0;i<reset3.length;i++)
 	      reset3[i].checked = false;
 	answerThree.textContent ="";
 }
